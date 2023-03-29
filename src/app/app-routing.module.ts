cb245c26fc098e640b3dd72fde3290f73b053d5d
Alpha <jcarirojas@safareyes.es>
2023-03-29 19:13:06 +0200

    CLASE-LOGIN

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {LoginComponent} from "./login/login.component";
+import {RegistroComponent} from "./registro/registro.component";
+import {ContactosComponent} from "./contactos/contactos.component";
+import {PerfilComponent} from "./perfil/perfil.component";
 
 const routes: Routes = [
 
-  {path:'home', component:HomeComponent}
+  {path:'', component:HomeComponent},
+  {path:'login', component:LoginComponent},
+  {path:'registro', component:RegistroComponent},
+  {path:'contacto', component:ContactosComponent},
+  {path:'perfil', component:PerfilComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContentComponent } from './content/content.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RegistroComponent } from './registro/registro.component';
+import { ContactosComponent } from './contactos/contactos.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     FooterComponent,
-    ContentComponent
+    ContentComponent,
+    LoginComponent,
+    NavbarComponent,
+    RegistroComponent,
+    ContactosComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
